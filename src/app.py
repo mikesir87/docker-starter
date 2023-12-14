97795efca73afcb697479898533923a91989de37
Michael Irwin <mikesir87@gmail.com>
2023-12-14 09:41:52 -0500

    Revert "Containerize and add GitHub Actions"
    This reverts commit 07675c2f9cb299c9c8f971ee78938efab7c8c64a.

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'superSecret'
 app.config['MYSQL_DATABASE_DB'] = 'memes'
-app.config['MYSQL_DATABASE_HOST'] = 'db'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_CURSORCLASS'] = pymysql.cursors.DictCursor
 mysql.init_app(app)
 
@@ -15,7 +15,7 @@ mysql.init_app(app)
 # The HTML returned from here will be sent to the browser making the request.
 @app.route("/")
 def index():
-    message = "Hello world!?"
+    message = "Hello world!"
     cursor = mysql.get_db().cursor()
     cursor.execute("SELECT * FROM memes ORDER BY RAND() LIMIT 1");
     row = cursor.fetchone()
